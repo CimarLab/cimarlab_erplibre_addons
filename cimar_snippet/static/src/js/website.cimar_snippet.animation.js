d9eadc0041b3a93666bbfacc9df8358d72e746ec
Mathieu Benoit <mathben@technolibre.ca>
2022-04-26 16:15:45 -0400

    [UPD] cimar_snippet: add destroyer before save snippet

--- cimar_snippet/static/src/js/website.cimar_snippet.animation.js
+++ cimar_snippet/static/src/js/website.cimar_snippet.animation.js
@@ -1,14 +1,16 @@
 odoo.define("cimar_snippet.animation", function (require) {
     "use strict";
 
-    var sAnimation = require("website.content.snippets.animation");
+    let sAnimation = require("website.content.snippets.animation");
 
     sAnimation.registry.cimar_snippet = sAnimation.Class.extend({
         selector: ".o_cimar_snippet",
 
         start: function () {
-            var self = this;
-            var def = this._rpc({ route: "/cimar_snippet/helloworld" }).then(
+            let self = this;
+            this._eventList = this.$(".cimar_snippet_value");
+            this._originalContent = this._eventList.text();
+            let def = this._rpc({ route: "/cimar_snippet/helloworld" }).then(
                 function (data) {
                     if (data.error) {
                         return;
@@ -18,13 +20,18 @@ odoo.define("cimar_snippet.animation", function (require) {
                         return;
                     }
 
-                    var data_json = data;
-                    var hello = data_json["hello"];
-                    self.$(".cimar_snippet_value").text(hello);
+                    self._$loadedContent = $(data);
+                    self._eventList.text(data["hello"]);
                 }
             );
 
             return $.when(this._super.apply(this, arguments), def);
+        },
+        destroy: function () {
+            this._super.apply(this, arguments);
+            if (this._$loadedContent) {
+                this._eventList.text(this._originalContent);
+            }
         },
     });
 });
